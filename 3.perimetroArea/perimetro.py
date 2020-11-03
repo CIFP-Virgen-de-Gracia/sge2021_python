b6eb076ea84e907db6e1e9566f744deffe70ad17
inmagijon <igijoninf@gmail.com>
2020-11-03 17:31:21 +0100

    Added "Hello World" examples

---
+++ 3.perimetroArea/perimetro.py
@@ -0,0 +1,15 @@
+
+print("Vamos a calcular el perímetro y área de un cuadrado")
+lado = int(input("Introduzca el lado: "))
+perimetro = lado*4
+area = lado**2
+print("Perímetro: ",perimetro," Área: ",area)
+
+print("Vamos a calcular el perímetro y área de un cubo")
+lado = int(input("Introduzca el lado: "))
+perimetro = lado*6
+area = (lado**2)*6
+print("Perímetro: ",perimetro," Área: ",area)
+
+
+
